3c2efd7fc9646f3bf6fae31d2b6e88a3b27073a8
zac nation <zac.nation@icloud.com>
2023-08-01 20:57:13 +0100

    419: Set up Mongoose + make campground model

--- app.js
+++ app.js
@@ -1,12 +1,36 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+const Campground = require('./models/campground');
 
-app.set('view engine', 'ejs');
+mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp', {
+  useNewUrlParser: true,
+  // useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error'));
+db.once('open', () => {
+  console.log('Database connected');
+});
+
+const app = express();
+
 app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
   res.render('home');
+});
+
+app.get('/makecampground', async (req, res) => {
+  const camp = new Campground({
+    title: 'My Backyard',
+    description: 'cheap camping!',
+  });
+  await camp.save();
+  res.send(camp);
 });
 
 app.listen(3000, () => {
