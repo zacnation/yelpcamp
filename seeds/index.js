d242ad715f3a6b290e3af14a1d2e1ef71312b37b
zac nation <zac.nation@icloud.com>
2023-08-03 18:24:51 +0100

    438: Update campground model and reseed db + Add image and description to show page

--- seeds/index.js
+++ seeds/index.js
@@ -21,9 +21,14 @@ const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
+      image: 'https://source.unsplash.com/collection/483251/',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet nam voluptatum vel beatae sequi, enim eveniet fugiat sit sint dolor quibusdam quos deserunt accusamus. Quos libero dolorem enim et odio.',
+      price,
     });
     await camp.save();
   }
