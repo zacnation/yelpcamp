1c8b393d7cb87188e1e2f454446f6883fc949303
zac nation <zac.nation@icloud.com>
2023-08-11 16:43:52 +0100

    500: Serve static assets

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.render('home');
