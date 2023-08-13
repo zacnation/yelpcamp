dd51811ca3006f6dfd37e26a0dc2f5c08446ebb2
zac nation <zac.nation@icloud.com>
2023-08-13 12:46:13 +0100

    520: Configure Passport

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const session = require('express-session');
 const flash = require('connect-flash');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
 
 const campgrounds = require('./routes/campgrounds');
 const reviews = require('./routes/reviews');
@@ -46,18 +49,31 @@ const sessionConfig = {
 app.use(session(sessionConfig));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req, res, next) => {
   res.locals.success = req.flash('success');
   res.locals.error = req.flash('error');
   next();
 });
 
+app.use('/campgrounds', campgrounds);
+app.use('/campgrounds/:id/reviews', reviews);
+
 app.get('/', (req, res) => {
   res.render('home');
 });
 
-app.use('/campgrounds', campgrounds);
-app.use('/campgrounds/:id/reviews', reviews);
+app.get('/fakeUser', async (req, res) => {
+  const user = new User({ email: 'zac@test.com', username: 'zaccc' });
+  const newUser = await User.register(user, 'password');
+  res.send(newUser);
+});
 
 app.all('*', (req, res, next) => {
   next(new ExpressError('Page Not Found', 404));
