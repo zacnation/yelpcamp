d335f1392822718b696a877c681590476e034dae
zac nation <zac.nation@icloud.com>
2023-08-16 07:06:45 +0100

    543: Add Multer middleware

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -3,17 +3,20 @@ const router = express.Router();
 const campgrounds = require('../controllers/campgrounds');
 const catchAsync = require('../utils/catchAsync');
 const { validateCampground, isLoggedIn, isAuthor } = require('../middleware');
-const ExpressError = require('../utils/ExpressError');
-const Campground = require('../models/campground');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
 
 router
   .route('/')
   .get(catchAsync(campgrounds.index))
-  .post(
-    isLoggedIn,
-    validateCampground,
-    catchAsync(campgrounds.createCampground)
-  );
+  // .post(
+  //   isLoggedIn,
+  //   validateCampground,
+  //   catchAsync(campgrounds.createCampground))
+  .post(upload.array('image'), (req, res) => {
+    console.log(req.body, req.files);
+    res.send(':)');
+  });
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
