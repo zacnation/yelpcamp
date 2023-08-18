0c515d90473df8dd1b5aba72174c4971053d6f56
zac nation <zac.nation@icloud.com>
2023-08-18 15:57:16 +0100

    576: Add map controls

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,10 +1,13 @@
 mapboxgl.accessToken = mapToken;
+
 const map = new mapboxgl.Map({
   container: 'map', // container ID
   style: 'mapbox://styles/mapbox/streets-v12', // style URL
   center: campground.geometry.coordinates, // starting position [lng, lat]
   zoom: 10, // starting zoom
 });
+
+map.addControl(new mapboxgl.NavigationControl());
 
 new mapboxgl.Marker()
   .setLngLat(campground.geometry.coordinates)
