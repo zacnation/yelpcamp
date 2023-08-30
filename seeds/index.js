49546c713de0a16b4c91c1a8a26db502f51200d1
zac nation <zac.nation@icloud.com>
2023-08-30 12:15:41 +0100

    Cyclic - revert to local db

--- seeds/index.js
+++ seeds/index.js
@@ -3,7 +3,7 @@ const cities = require('./cities');
 const { places, descriptors } = require('./seedHelpers');
 const Campground = require('../models/campground');
 
-mongoose.connect(`mongodb://127.0.0.1:27017/${process.env.DB_URL}`, {
+mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp', {
   useNewUrlParser: true,
   // useCreateIndex: true,
   useUnifiedTopology: true,
@@ -54,5 +54,3 @@ const seedDB = async () => {
 seedDB().then(() => {
   mongoose.connection.close();
 });
-
-//
