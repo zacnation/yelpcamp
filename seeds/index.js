87d9e0a8f241f6f372f0e37f312911e3984556bc
zac nation <zac.nation@icloud.com>
2023-08-30 12:52:22 +0100

    Cyclic - seed db

--- seeds/index.js
+++ seeds/index.js
@@ -3,7 +3,11 @@ const cities = require('./cities');
 const { places, descriptors } = require('./seedHelpers');
 const Campground = require('../models/campground');
 
-const dbUrl = process.env.DB_URL || 'mongodb://127.0.0.1:27017/yelp-camp';
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 
 mongoose.connect(dbUrl, {
   useNewUrlParser: true,
